94c20195c7d077554c62872dd9d5784ec42c4717
meodingu <lelepi135246@gmail.com>
2023-12-30 14:22:16 +0700

    done cong_no

--- controllers/reportC.js
+++ controllers/reportC.js
@@ -74,7 +74,7 @@ class reportC {
           let index = allBooks.findIndex((book) => {
             return book.MaSach === donNhapSachTuTruoc[i].MaSach;
           });
-          allBooks[index].TonDau += donNhapSachTuTruoc[i].SoLuong;
+          allBooks[index].TonDau += donNhapSachTuTruoc[i].SoLzuong;
         }
 
         for (let i = 0; i < donNhapSach.length; i++) {
@@ -143,22 +143,23 @@ class reportC {
       if (option === 2) {
         // LAY ALL BILL MA THU NO THANG NAY
         const HDThuNo = await HoaDon.selectHoaDonGN(month, year);
-
+        console.log(HDThuNo)
         // lay ALL PHIEU THU NO THANG NAY
         const PTN = await PhieuThuTien.selectDebt(month, year);
-
+        console.log(PTN);
         // lay HET KHACH HANG
         let allCustomers = await Customer.selectAllCustomers();
 
         // LAY ALL HOA DON GHI NO TU TRUOC DEN TRUOC THANG NAY
         let HDThuNoTuTruoc = await HoaDon.selectHoaDonGNTuTruoc(result);
-
+        console.log(HDThuNoTuTruoc);
         // lay ALL PHIEU THU NO TU TRUOC DEN THANG NAY
         const PTNTuTruoc = await PhieuThuTien.selectDebtTuTruoc(result);
-
+        console.log(PTNTuTruoc);
         for (let i = 0; i < allCustomers.length; i++) {
           allCustomers[i] = {
-            ...allCustomers[i],
+              ...allCustomers[i],
+              SDT: allCustomers[i].SDT,
             NoDau: 0,
             NoCuoi: 0,
             No: 0,
@@ -166,78 +167,66 @@ class reportC {
             PhatSinh: 0,
           };
         }
-        // console.log("donNSTT: ", donNhapSachTuTruoc);
-          // console.log("HDTT: ", hoadonTuTruoc);
-          
 
  /////////////////////////////////////////////////// STOP HERE///////////////////////////////////////////
-        for (let i = 0; i < HDThuNoTuTruoc.length; i++) {
-          let index = allCustomers.findIndex((customer) => {
-            return customer.MaSach === HDThuNoTuTruoc[i].MaSach;
-          });
-          allBooks[index].TonDau += donNhapSachTuTruoc[i].SoLuong;
-        }
+        
+    for (let i = 0; i < HDThuNoTuTruoc.length; i++) {   // oke
+      let index = allCustomers.findIndex((customer) => {
+        return customer.MaKH === HDThuNoTuTruoc[i].KhachHang;
+      });
+        allCustomers[index].NoDau += HDThuNoTuTruoc[i].ThanhTien;
 
-        for (let i = 0; i < donNhapSach.length; i++) {
-          const dsTTDonNhapSach = await Book.selectThongTinNhapSach(
-            donNhapSach[i].MaDonNS
-          );
-          for (let j = 0; j < dsTTDonNhapSach.length; j++) {
-            // console.log("TTNS:", dsTTDonNhapSach[j].MaSach);
-            let index = allBooks.findIndex((book) => {
-              return book.MaSach === dsTTDonNhapSach[j].MaSach;
-            });
-            // console.log("index = ", index);
-            allBooks[index].Nhap += dsTTDonNhapSach[j].SoLuong;
-          }
-        }
-        // console.log(allBooks);
+        // console.log("no tu truoc")
+        // console.log(allCustomers[index].NoDau, allCustomers);
+        // console.log("no tu truoc");
 
-        for (let i = 0; i < hoadonTuTruoc.length; i++) {
-          let index = allBooks.findIndex((book) => {
-            return book.MaSach === hoadonTuTruoc[i].MaSach;
-          });
-          allBooks[index].TonDau -= hoadonTuTruoc[i].SoLuong;
-        }
+    }
 
-        console.log("HD:", hoadon);
+    for (let i = 0; i < PTN.length; i++) {  // may be oke
+      let index = allCustomers.findIndex((customer) => {
+        return customer.MaKH === PTN[i].KhachHang;
+      });
+        console.log("so tien tra trong thang nay");
+        allCustomers[index].Tra += PTN[i].SoTienThu;
+        console.log("so tien tra trong thang nay");
+    }
 
-        for (let i = 0; i < hoadon.length; i++) {
-          const hdTT = await HoaDon.selectTTHoaDon(hoadon[i].MaHoaDon);
-          console.log("HdTT", hdTT);
-          for (let j = 0; j < hdTT.length; j++) {
-            // console.log("TTNS:", dsTTDonNhapSach[j].MaSach);
-            let index = allBooks.findIndex((book) => {
-              return book.MaSach === hdTT[j].MaSach;
-            });
-            // console.log("index = ", index);
-            allBooks[index].Ban += hdTT[j].SoLuong;
-          }
-        }
-        for (let i = 0; i < allBooks.length; i++) {
-          allBooks[i].PhatSinh = allBooks[i].Nhap - allBooks[i].Ban;
-          allBooks[i].TonCuoi = allBooks[i].TonDau + allBooks[i].PhatSinh;
-        }
-        console.log(allBooks);
+
+    for (let i = 0; i < PTNTuTruoc.length; i++) {   // oke
+      let index = allCustomers.findIndex((customer) => {
+        return customer.MaKH === PTNTuTruoc[i].KhachHang;
+      });
+        allCustomers[index].NoDau -= PTNTuTruoc[i].SoTienThu;
+        // console.log("Sau khi tra no all thang truoc");
+        // console.log(allCustomers[index].NoDau)
+        // console.log("Sau khi tra no all thang truoc")
+    }
+
+    for (let i = 0; i < HDThuNo.length; i++) {
+      let index = allCustomers.findIndex((customer) => {
+        return customer.MaKH === HDThuNo[i].KhachHang;
+      });
+        console.log("So tien no trong thang nay")
+        allCustomers[index].No += HDThuNo[i].ThanhTien;
+        console.log("So tien no trong thang nay");
+    }
+
+    for (let i = 0; i < allCustomers.length; i++) {
+      allCustomers[i].PhatSinh = allCustomers[i].No - allCustomers[i].Tra;
+      allCustomers[i].NoCuoi = allCustomers[i].NoDau + allCustomers[i].PhatSinh;
+    }
+
+    console.log(allCustomers);
         let hasEmpty = true;
-        // for (let i = 0; i < allBooks.length; i++) {
-        //     if (allBooks[i].TonDau <= allBooks[i].TonCuoi) {
-        //         hasEmpty = true;
-        //     }
-        // }
-
-        // allBooks = {...allBooks, month: month, year: year};
-
         let data = {
-          books: allBooks,
+          customers: allCustomers,
           month: month,
           year: year,
         };
-
         res.render("report", {
           empty: hasEmpty,
-          allBooks: data,
-          option: 1,
+          allCustomers: data,
+          option: 2,
         });
       }
     } catch (error) {
